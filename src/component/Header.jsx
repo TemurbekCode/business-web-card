6286e8f0e2996e2116e9f14f316dc5c1adfc3190
Temur <243559364+TemurbekCode@users.noreply.github.com>
2026-05-11 12:18:49 +0300

    update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -10,18 +10,18 @@ function Header() {
             <h1 className="name">Temur Alisherov</h1><br /><br />
 
             <div className="socials">
-                <ul class="example-2">
-                    <li class="icon-content">
+                <ul className="example-2">
+                    <li className="icon-content">
                         <a
                             data-social="linkedin"
                             aria-label="LinkedIn"
                             href="https://www.linkedin.com/in/temurbek-alisherov-42a5b23b3?utm_source=share_via&utm_content=profile&utm_medium=member_android"
                         >
-                            <div class="filled"></div>
+                            <div className="filled"></div>
                             <svg
                                 xml:space="preserve"
                                 viewBox="0 0 16 16"
-                                class="bi bi-linkedin"
+                                className="bi bi-linkedin"
                                 fill="currentColor"
                                 height="16"
                                 width="16"
@@ -34,13 +34,13 @@ function Header() {
                             </svg>
                         </a>
                     </li>
-                    <li class="icon-content">
+                    <li className="icon-content">
                         <a data-social="github" aria-label="GitHub" href="https://github.com/TemurbekCode">
-                            <div class="filled"></div>
+                            <div className="filled"></div>
                             <svg
                                 xml:space="preserve"
                                 viewBox="0 0 16 16"
-                                class="bi bi-github"
+                                className="bi bi-github"
                                 fill="currentColor"
                                 height="16"
                                 width="16"
@@ -53,17 +53,17 @@ function Header() {
                             </svg>
                         </a>
                     </li>
-                    <li class="icon-content">
+                    <li className="icon-content">
                         <a
                             data-social="instagram"
                             aria-label="Instagram"
                             href="https://www.instagram.com/alisherovdev?igsh=MXg2aDB1M3RqdXdvcw=="
                         >
-                            <div class="filled"></div>
+                            <div className="filled"></div>
                             <svg
                                 xml:space="preserve"
                                 viewBox="0 0 16 16"
-                                class="bi bi-instagram"
+                                className="bi bi-instagram"
                                 fill="currentColor"
                                 height="16"
                                 width="16"
@@ -76,13 +76,13 @@ function Header() {
                             </svg>
                         </a>
                     </li>
-                    <li class="icon-content">
+                    <li className="icon-content">
                         <a data-social="youtube" aria-label="Youtube" href="https://youtube.com/@tim_game?si=Yx5jJpdbzSdvg1hw">
-                            <div class="filled"></div>
+                            <div className="filled"></div>
                             <svg
                                 xml:space="preserve"
                                 viewBox="0 0 16 16"
-                                class="bi bi-youtube"
+                                className="bi bi-youtube"
                                 fill="currentColor"
                                 height="16"
                                 width="16"
@@ -97,14 +97,14 @@ function Header() {
                     </li>
                 </ul>
 
-                <ul class="example-2">
-                    <li class="icon-content">
+                <ul className="example-2">
+                    <li className="icon-content">
                         <a
                             href="https://t.me/alisherovdevs"
                             aria-label="Telegram"
                             data-social="telegram"
                         >
-                            <div class="filled"></div>
+                            <div className="filled"></div>
                             <svg version="1.1" viewBox="0 0 100 100">
                                 <path
                                     d="M95,9.9c-1.3-1.1-3.4-1.2-7-0.1c0,0,0,0,0,0c-2.5,0.8-24.7,9.2-44.3,17.3c-17.6,7.3-31.9,13.7-33.6,14.5  c-1.9,0.6-6,2.4-6.2,5.2c-0.1,1.8,1.4,3.4,4.3,4.7c3.1,1.6,16.8,6.2,19.7,7.1c1,3.4,6.9,23.3,7.2,24.5c0.4,1.8,1.6,2.8,2.2,3.2  c0.1,0.1,0.3,0.3,0.5,0.4c0.3,0.2,0.7,0.3,1.2,0.3c0.7,0,1.5-0.3,2.2-0.8c3.7-3,10.1-9.7,11.9-11.6c7.9,6.2,16.5,13.1,17.3,13.9  c0,0,0.1,0.1,0.1,0.1c1.9,1.6,3.9,2.5,5.7,2.5c0.6,0,1.2-0.1,1.8-0.3c2.1-0.7,3.6-2.7,4.1-5.4c0-0.1,0.1-0.5,0.3-1.2  c3.4-14.8,6.1-27.8,8.3-38.7c2.1-10.7,3.8-21.2,4.8-26.8c0.2-1.4,0.4-2.5,0.5-3.2C96.3,13.5,96.5,11.2,95,9.9z M30,58.3l47.7-31.6  c0.1-0.1,0.3-0.2,0.4-0.3c0,0,0,0,0,0c0.1,0,0.1-0.1,0.2-0.1c0.1,0,0.1,0,0.2-0.1c-0.1,0.1-0.2,0.4-0.4,0.6L66,38.1  c-8.4,7.7-19.4,17.8-26.7,24.4c0,0,0,0,0,0.1c0,0-0.1,0.1-0.1,0.1c0,0,0,0.1-0.1,0.1c0,0.1,0,0.1-0.1,0.2c0,0,0,0.1,0,0.1  c0,0,0,0,0,0.1c-0.5,5.6-1.4,15.2-1.8,19.5c0,0,0,0,0-0.1C36.8,81.4,31.2,62.3,30,58.3z"
@@ -113,8 +113,8 @@ function Header() {
                             </svg>
                         </a>
                     </li>
-                    <ul class="example-2">
-                        <li class="icon-content">
+                    <ul className="example-2">
+                        <li className="icon-content">
                             <a className="web-link" href="https://portfolio-temurbek.netlify.app/">
                                 {/* 🌐 */}
                                 <img src="globe.png" className="globe"  />
